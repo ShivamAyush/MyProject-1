832812aac5897b2d28d350956906e1231440f8cb
ShivamAyush <52344946+ShivamAyush@users.noreply.github.com>
2020-08-06 10:38:13 +0530

    Update VideoGame.java

--- EmployeeManagementSystem/src/com/lti/VideoGame.java
+++ EmployeeManagementSystem/src/com/lti/VideoGame.java
@@ -4,7 +4,7 @@ public class VideoGame {
 	public void pubg(){
 		System.out.println("pubg is working...");
 		System.out.println("Yo");
-		System.out.println("yo");
+		System.out.println("yo Honey Singh");
 	}
 
 	public static void main(String[] args) {
