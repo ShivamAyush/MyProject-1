752cffee7bc9f78cdc2a25d9724e0e1b2eae02c4
Ankur4856 <araj3358@gmail.com>
2020-08-06 10:16:42 +0530

    Implemented pubg

--- EmployeeManagementSystem/src/com/lti/VideoGame.java
+++ EmployeeManagementSystem/src/com/lti/VideoGame.java
@@ -3,7 +3,7 @@ package com.lti;
 public class VideoGame {
 	public void pubg(){
 		System.out.println("pubg is working...");
-		
+		System.out.println("Yo");
 	}
 
 	public static void main(String[] args) {
